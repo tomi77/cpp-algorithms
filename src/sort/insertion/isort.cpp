ba2dcd44ae3d15ad336b7522a0b97131d0eba550
Tomasz Rup <tomasz.r@elcar-gps.com.pl>
2014-11-05 17:24:54 +0100

    fix insertion sort

--- src/sort/insertion/isort.cpp
+++ src/sort/insertion/isort.cpp
@@ -10,12 +10,12 @@ namespace sort {
 		unsigned int i, j;
 		int k;
 
-		for (i = 0; i < v.size(); ++i)
+		for (i = 1; i < v.size(); ++i)
 		{
 			j = i;
 			k = v[i];
 
-			while (v[j - 1] > k && j > 0)
+			while (j > 0 && v[j - 1] > k)
 			{
 				v[j] = v[j - 1];
 				--j;
