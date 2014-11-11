1d50ac35d5f2dbe02412fe74eedafddca543e05a
Tomasz Rup <tomasz.r@elcar-gps.com.pl>
2014-11-11 21:41:52 +0100

    refactoring

--- src/crypto/caesar/caesar.cpp
+++ src/crypto/caesar/caesar.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 namespace crypto {
 	namespace caesar {
-		void encode(string &s, int k)
+		void caesar(string &s, int k)
 		{
 			char c;
 			string::iterator iter = s.begin();
@@ -18,21 +18,21 @@ namespace crypto {
 				{
 					c -= 'a';
 					c += k;
-					c = fmod(c, 'z' - 'a' + 1);
+					c = c < 0 ? 'z' - 'a' + 1 - fabs(c) : fmod(c, 'z' - 'a' + 1);
 					c += 'a';
 				}
 				else if (c >= 'A' && c <= 'Z')
 				{
 					c -= 'A';
 					c += k;
-					c = fmod(c, 'Z' - 'A' + 1);
+					c = c < 0 ? 'Z' - 'A' + 1 - fabs(c) : fmod(c, 'Z' - 'A' + 1);
 					c += 'A';
 				}
 				else if (c >= '0' && c <= '9')
 				{
 					c -= '0';
 					c += k;
-					c = fmod(c, '9' - '0' + 1);
+					c = c < 0 ? '9' - '0' + 1 - fabs(c) : fmod(c, '9' - '0' + 1);
 					c += '0';
 				}
 
@@ -40,56 +40,26 @@ namespace crypto {
 
 				iter++;
 			}
+		}
+
+		void encode(string &s, int k)
+		{
+			caesar(s, k);
 		}
 
 		void encode(string &s)
 		{
-			encode(s, 3);
+			caesar(s, 3);
 		}
 
 		void decode(string &s, int k)
 		{
-			char c;
-			string::iterator iter = s.begin();
-
-			while (iter != s.cend())
-			{
-				c = *iter;
-
-				if (c >= 'a' && c <= 'z')
-				{
-					c -= 'a';
-					c -= k;
-					if (c < 0)
-						c = 'z' - 'a' + 1 - fabs(c);
-					c += 'a';
-				}
-				else if (c >= 'A' && c <= 'Z')
-				{
-					c -= 'A';
-					c -= k;
-					if (c < 0)
-						c = 'Z' - 'A' + 1 - fabs(c);
-					c += 'A';
-				}
-				else if (c >= '0' && c <= '9')
-				{
-					c -= '0';
-					c -= k;
-					if (c < 0)
-						c = '9' - '0' + 1 - fabs(c);
-					c += '0';
-				}
-
-				*iter = c;
-
-				iter++;
-			}
+			caesar(s, -k);
 		}
 
 		void decode(string &s)
 		{
-			decode(s, 3);
+			caesar(s, -3);
 		}
 	}
 }
